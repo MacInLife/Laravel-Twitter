e61e31941e74b7695d7e095d03ba002b9d8e117d
Mac In Life <marie.ange.coco@gmail.com>
2020-02-20 16:46:42 +0100

    Fermeture personne à suivre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,18 +8,38 @@
             <div class="alert alert-success alert-dismissible">{!! session('ok') !!}</div>
             @endif
             <div class="card mb-2">
-                <div class="card-header">Personne que vous pouvez suivre</div>
-                @foreach ($users as $user)
-                <div class="card-body d-flex py-2">
-                    <div class="mr-2 float-left" style="width:80px;"><img class="m-auto rounded img-thumbnail"
-                            src="{{$user->getAvatar()}}" width="100%" height="100%">
-                    </div>
-                    <p class="mr-auto p-2 my-auto">{{$user->name}}</p>
-                    <div class="p-2">
-                        <a href="#" class="btn btn-primary btn-lg" role="button" aria-pressed="true">Follow</a>
+                <div class="navbar px-0 bg-light" style="
+    border-bottom: 1px solid lightgrey;">
+                    <h6 class=" mt-2 pl-4">Personne que vous pouvez suivre</h6>
+                    <button class="navbar-toggler mr-4" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <svg width="20" height="20" aria-hidden="true" focusable="false" data-prefix="fas"
+                            data-icon="angle-down" class="svg-inline--fa fa-angle-down fa-w-10" role="img"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                            <path fill="currentColor"
+                                d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z">
+                            </path>
+                        </svg>
+                    </button>
+                    <div class="collapse navbar-collapse bg-white p-2" id="navbarSupportedContent">
+                        <div class="content p-2">
+                            @foreach ($users as $user)
+                            <div class="card-body d-flex p-2">
+                                <div class="mr-2 float-left" style="width:80px;"><img
+                                        class="m-auto rounded img-thumbnail" src="{{$user->getAvatar()}}" width="100%"
+                                        height="100%">
+                                </div>
+                                <p class="mr-auto p-2 my-auto">{{$user->name}}</p>
+                                <div class="p-2 my-auto">
+                                    <a href="#" class="btn btn-primary btn-lg" role="button"
+                                        aria-pressed="true">Follow</a>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-                @endforeach
             </div>
 
             <div class="d-flex">
